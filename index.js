0f958e28136e5a648c7274b8690aba685c471d2f
Knox635 <joshuadubouzet12@gmail.com>
2024-09-05 15:35:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Listening to Spotify","Playing VALORANT"];[MADE IN GPO MIDDLEMANSERVICES],
 
 
 let currentIndex = 0;
